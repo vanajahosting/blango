c83e556140128b8eada076fe7c1b65094517c65e
Ernesto Hernandez <ernesto.hernandezbarrios@metropolia.fi>
2024-02-17 16:30:21 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
 ]
 
@@ -133,3 +135,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
--- blango/urls.py
+++ blango/urls.py
@@ -19,7 +19,7 @@ import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", blog.views.index),
-    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path('', blog.views.index),
+    path('post/<slug>/', blog.views.post_detail, name="blog-post-detail"),
 
 ]
